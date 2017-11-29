e64575004a9dc37771db4ec6e2d6f75a234375e7
Ian Wakeling <ian@lihb.me.uk>
2017-11-29 22:28:16 +0000

    Configure internal pull down resistor

--- yardmaster.py
+++ yardmaster.py
@@ -10,28 +10,30 @@ BLACK       = (   0,   0,   0 )
 RED         = ( 255,   0,   0 )
 GREEN       = (   0, 255,   0 )
 DARKGREEN   = (   0, 128,   0 )
+LIGHTGREEN  = (   0, 164,   0 )
 DARKYELLOW  = ( 128, 128,  64 )
-DARKRED     = ( 128,   0,   0 )
-DARKGRAY    = (  64,  64,  64 )
+LIGHTYELLOW = ( 164, 164, 128 )
+DARKRED     = ( 164,   0,   0 )
+DARKGRAY    = (  96,  96,  96 )
 
 BGCOLOUR = BLACK
 
-SHUTDOWN_PIN = 11
-NEXT_MOVE_PIN = 12
+MAX_WAGON_WIDTH = 50
+WAGON_GAP = 5
+
+PREV_MOVE_PIN = int( 11 ) #blue
+NEXT_MOVE_PIN = int( 12 ) #green
+WAGON_SELECT_PIN = int( 13 ) #yellow
+WAGON_CHANGE_PIN = int( 16 ) #white
+SHUTDOWN_PIN = int( 15 ) #red
 
 TYPE = 0
 TIME = 1
 AM_PM = 2
 DESCRIPTION = 3
 
-class Wagon:
-    def __init__( self, load, age ):
-        self.load = load
-        self.age = age
-        self.outgoing = False
-
-    def markOutgoing( self ):
-        self.outgoing = True
+def wagonOutgoing( wagon ):
+    return wagon.outgoing
 
 def wagonRemaining( wagon ):
     return not wagon.outgoing
@@ -41,12 +43,75 @@ def parseVertex( vertex, game ):
     return (    int( coords[0] ),
                 int( coords[1] ) + game.height / 3 + 30 )
 
+def incrementIndex( currentValue, limit ):
+    newValue = currentValue + 1
+    if newValue >= limit:
+        newValue = 0
+    return newValue
+
+def setupButton( pin ):
+    GPIO.setup( pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN )
+    GPIO.add_event_detect( 
+        pin,
+        GPIO.RISING )
+
+def checkButton( pin ):
+    return GPIO.input( pin ) or GPIO.event_detected( pin )
+
+class Wagon:
+    def __init__( self, wagonType ):
+        self.wagonType = int( wagonType )
+        self.age = 0 
+        self.outgoing = False
+
+    def markOutgoing( self ):
+        self.outgoing = True
+
+    def text( self, wagonTypes ):
+        if self.wagonType < len( wagonTypes ):
+            val = wagonTypes[ self.wagonType ]
+        else:
+            val = '?'
+        return val
+
+    def draw( self, left, top, width, game ):
+        if self.age == 0:
+            colour = DARKGREEN
+        elif self.outgoing:
+            colour = DARKRED
+        else:
+            colour = DARKYELLOW
+        top = top - game.wagonFont.get_linesize()
+        height = game.wagonFont.get_linesize() + game.wagonFont.get_height()
+        text = self.text( game.wagonTypes )
+        pygame.draw.rect(
+            game.surface,
+            colour,
+            Rect( left, top, width, height ),
+            1 )
+        if width < 0:
+            left = left + width
+            space = -WAGON_GAP
+        else:
+            space = width + WAGON_GAP
+        game.drawText( 
+            text,
+            colour,
+            game.wagonFont,
+            left,
+            top,
+            math.fabs( width ) )
+        left = left + space
+        return left
+
 class Siding:
     def __init__( self, length, vertices, game ):
         self.length = length
         self.vertices = [   parseVertex( vertex, game ) 
                             for vertex in vertices.split( ';' ) ]
-        self.present = 0
+        self.displayLength = math.fabs(
+                                self.vertices[-1][0] 
+                                - self.vertices[0][0] )
         self.wagons = []
 
     def draw( self, game ):
@@ -58,29 +123,14 @@ class Siding:
             3 )
         left = self.vertices[0][0]
         top = self.vertices[0][1]
-        if left < self.vertices[1][0]:
-            factor = 1
-        else:
-            factor= -1
-        for wagon in self.wagons:
-            if wagon.age == 0:
-                colour = DARKGREEN
-            elif wagon.outgoing:
-                colour = DARKRED
-            else:
-                colour = DARKYELLOW
-            (width, height) = game.font.size( wagon.load )
-            if factor < 0:
-                left = left - width
-                width = 10
-            else:
-                width = width + 10
-            game.drawTextLine( 
-                wagon.load,
-                colour,
-                left,
-                top )
-            left = left + width * factor
+        if self.length > 0:
+            wagonWidth = ( self.displayLength / self.length ) - WAGON_GAP
+            if wagonWidth > MAX_WAGON_WIDTH:
+                wagonWidth = MAX_WAGON_WIDTH
+            if left > self.vertices[1][0]:
+                wagonWidth = -wagonWidth
+            for wagon in self.wagons:
+                left = wagon.draw( left, top, wagonWidth, game )
 
     def ageWagons( self ):
         for wagon in self.wagons:
@@ -92,7 +142,6 @@ class Siding:
             if len( oldest ) < trainLength:
                 oldest.append( wagon )
             else:
-                minAge = 0
                 youngest = oldest[0]
                 for candidate in oldest:
                     if candidate.age < youngest.age:
@@ -101,14 +150,17 @@ class Siding:
                     oldest.remove( youngest )
                     oldest.append( wagon )
 
-    def removeOutgoing( self ):
+    def transferOutgoing( self, rake ):
+        rake.extend( filter( wagonOutgoing, self.wagons ) )
         self.wagons = filter( wagonRemaining, self.wagons )
                         
 class Game:
     def __init__( self, baseName ):
         self.clock = pygame.time.Clock()
         self.moves = []
-        self.loads = []
+        self.wagonTypes = []
+        self.rakes = []
+        self.nextRake = 0
         self.sidings = []
         self.trainLength = 1
         self.lastMovePress = pygame.time.get_ticks()
@@ -118,15 +170,13 @@ class Game:
         self.surface = pygame.display.set_mode( ( 0, 0 ), pygame.FULLSCREEN )
         self.width = self.surface.get_width()
         self.height = self.surface.get_height()
-        #self.font = pygame.font.SysFont( 'freesansbold', 18, True )
         self.font = pygame.font.Font( 'freesansbold.ttf', 18 )
+        self.wagonFont = pygame.font.Font( 'freesansbold.ttf', 16 )
         pygame.display.set_caption( 'Yardmaster' )
         self.loadWTT( baseName )
         self.loadLayout( baseName )
         self.loadConfig( baseName )
         self.nextMoveTime()
-        self.dealCards( self.initialWagonCount )
-#        self.dialog( [ 'Press any key to begin' ] )
 
     def loadWTT( self, baseName ):
         f = open( baseName + ".wtt" )
@@ -148,43 +198,18 @@ class Game:
         for line in f:
             line = line.strip()
             if line != "":
-                (cmd,value,text) = line.split( '/' )
-                if cmd == 't':
-                    self.trainLength = int( value )
-                elif cmd == 'i':
-                    self.initialWagonCount = int( value )
-                else:
-                    for idx in range( 0, int( value ) ):
-                        self.loads.append( text )
+                fields = line.split( '/' )
+                if fields[0] == 'n':
+                    self.trainLength = int( fields[1] )
+                elif fields[0] == 'w':
+                    self.wagonTypes = fields[1:]
+                elif fields[0] == 'r':
+                    self.rakes.append( 
+                        map( Wagon, fields[1:] ) )
+                elif fields[0] == 'i':
+                    self.allocateWagons( 
+                        map( Wagon, fields[1:] ) )
         f.close()
-
-    def dialog( self, text ):
-        lines = map( 
-                    lambda line: self.font.render( line, True, DARKGRAY ),
-                    text )
-        rects = map(
-                    lambda line: line.get_rect(),
-                    lines )
-        total_height = reduce(
-                        lambda total, rect: total + rect.height,
-                        rects,
-                        0 )
-        vcenter = ( self.height / 2 ) - ( total_height / 2 )
-        for idx in range( 0, len( lines ) ):
-            rects[idx].center = ( self.width / 2, vcenter )
-            vcenter += rects[idx].height
-            self.surface.blit( lines[idx], rects[idx] )
-        pygame.display.update()
-        pygame.event.get( KEYUP )
-        pygame.time.wait( 500 )
-        res = 0 
-        while res == 0:
-            for event in  pygame.event.get():
-                if event.type == QUIT:
-                    res = 'q'
-                elif event.type == KEYUP:
-                    res = event.key
-        return res
 
     def start( self ):
         self.fps = 5
@@ -206,14 +231,13 @@ class Game:
             else:
                 self.moveTime = 0
 
-    def dealCards( self, trainLength ):
-        for wIdx in range( 0, trainLength ):
-            wagon = Wagon( random.choice( self.loads ), 0 ) 
+    def allocateWagons( self, train ):
+        for wagon in train:
             allocated = False
             while not allocated:
                 sIdx = random.randint( 0, len( self.sidings ) - 1 )
                 siding = self.sidings[ sIdx ]
-                if siding.present < siding.length:
+                if len( siding.wagons ) < siding.length:
                     allocated = True
                     siding.wagons.append( wagon )
 
@@ -228,16 +252,18 @@ class Game:
         for siding in self.sidings:
             siding.ageWagons() 
     
-    def removeOutgoing( self ):
+    def transferOutgoing( self, rake ):
         for siding in self.sidings:
-            siding.removeOutgoing() 
+            siding.transferOutgoing( rake ) 
+        for wagon in rake:
+            wagon.age = 0
+            wagon.outgoing = False
 
     def checkShutdownButton( self ):
-        return not GPIO.input( SHUTDOWN_PIN )
+        return ch
 
     def handleNextMoveButton( self ):
-        pressed = not GPIO.input( NEXT_MOVE_PIN )
-        if pressed:
+        if checkButton( NEXT_MOVE_PIN ):
             now = pygame.time.get_ticks()
             if now - self.lastMovePress > 1000:
                 self.moveIndex = self.moveIndex + 1
@@ -248,15 +274,19 @@ class Game:
                 else:
                     move = ""
                 if move != "":
-                   moveType = move.split( '/' )[ TYPE ] 
-                   if moveType == '-': 
-                       self.removeOutgoing()
-                   self.ageWagons()
-                   if moveType == '+':
-                       self.selectOutgoing()
-                       self.dealCards( self.trainLength )
-                   else:
-                       self.train = []
+                    rake = self.rakes[ self.nextRake ] 
+                    moveType = move.split( '/' )[ TYPE ] 
+                    if moveType == '-': 
+                        self.transferOutgoing( rake )
+                        self.nextRake = incrementIndex( 
+                                            self.nextRake,
+                                            len( self.rakes ) )
+                    self.ageWagons()
+                    if moveType == '+':
+                        self.selectOutgoing()
+                        self.allocateWagons( rake )
+                        rake[:] = []
+                        
 
     def drawClock( self, moveIndex ):
         clockSize = ( self.height / 6 ) - 10 
@@ -308,62 +338,83 @@ class Game:
             clockCenter,
             minPos )
 
-        self.drawTextLine( 
-            "t={}, ct={}".format( self.time, self.moveTime ),
-            WHITE,
-            10,
-            self.height / 3 + 10 )
-
-    def drawTextLine( self, text, colour, left, top ):
-        block = self.font.render( text, True, colour )
+    def drawTextLine( self, text, colour, font, left, top ):
+        block = font.render( text, True, colour )
         blockRect = block.get_rect()
         blockRect.topleft = ( left, top )
         self.surface.blit( block, blockRect )
 
-    def drawText( self, text, colour, left, top ):
+    def drawText( self, text, colour, font, left, top, maxWidth ):
         words = text.split()
         line = ""
         for idx in range( 0, len( words ) ):
             test = line + ' ' + words[ idx ]
-            (width, height) = self.font.size( test )
-            if left + width > self.width:
-                self.drawTextLine( line, colour, left, top )
-                line = ' ' + words[ idx ]
-                top = top + self.font.get_linesize()
+            (width, height) = font.size( test )
+            if left + width > maxWidth:
+                if line != "":
+                    self.drawTextLine( line, colour, font, left, top )
+                    line = ' ' + words[ idx ]
+                else:
+                    self.drawTextLine( words[ idx ], colour, font, left, top )
+                top = top + font.get_linesize()
             else:
                 line = test
         if line != "":
-            self.drawTextLine( line, colour, left, top )
+            self.drawTextLine( line, colour, font, left, top )
 
-    def drawLine( self, moveIndex, colour, slot ):
+    def drawMove( self, moveIndex, colour, slot ):
         move = self.moves[ moveIndex ].strip()
         if move != "":
             (newCards,time,ampm,description) = move.split( '/' ) 
             self.drawTextLine( 
                 time, 
-                colour, 
+                colour,
+                self.font,
                 self.height / 3, 
                 ( self.height / 9 ) * slot )
             self.drawTextLine( 
                 ampm, 
-                colour, 
+                colour,
+                self.font,
                 self.height / 3 + 40,
                 ( self.height / 9 ) * slot )
             self.drawText( 
                 description, 
-                colour, 
+                colour,
+                self.font,
                 self.height / 3 + 75, 
-                ( self.height / 9 ) * slot )
+                ( self.height / 9 ) * slot,
+                self.width )
+
+    def drawRakes( self, top ):
+        for rake in self.rakes:
+            height = 0
+            left = 10
+            for wagon in rake:
+                text = wagon.text( self.wagonTypes )
+                (width, height) = self.font.size( text )
+                if rake == self.rakes[ self.nextRake ]:
+                    colour = LIGHTYELLOW
+                else:
+                    colour = DARKGRAY
+                self.drawTextLine( 
+                    text,
+                    colour,
+                    self.font,
+                    left,
+                    top )
+                left = left + width + 10 
+            top = top + height
 
     def drawBoard( self ):
         left = 0
         self.surface.fill( BGCOLOUR )
         self.drawClock( self.moveIndex )
         if self.moveIndex > 0:
-            self.drawLine( self.moveIndex - 1, DARKGRAY, 0 )
-        self.drawLine( self.moveIndex, WHITE, 1 )
+            self.drawMove( self.moveIndex - 1, DARKGRAY, 0 )
+        self.drawMove( self.moveIndex, WHITE, 1 )
         if ( self.moveIndex + 1 ) < len( self.moves ):
-            self.drawLine( self.moveIndex + 1, DARKGRAY, 2 )
+            self.drawMove( self.moveIndex + 1, DARKGRAY, 2 )
         pygame.draw.line(
             self.surface,
             WHITE,
@@ -371,12 +422,18 @@ class Game:
             ( self.width - 5, self.height / 3 ) )
         for siding in self.sidings:
             siding.draw( self )
-
+        top = self.height / 6 *5
+        pygame.draw.line(
+            self.surface,
+            WHITE,
+            ( 5, top ),
+            ( self.width - 5, top ) )
+        self.drawRakes( top + 10 )
 
     def runGame( self ):
         done = False
         while not done:
-            done = self.checkShutdownButton()
+            done = checkButton( SHUTDOWN_PIN )
             self.handleNextMoveButton()
             if self.moveIndex >= len( self.moves ):
                 self.moveIndex = 0
@@ -390,9 +447,14 @@ class Game:
 
 def main():
     GPIO.setmode( GPIO.BOARD )
-    GPIO.setup( SHUTDOWN_PIN, GPIO.IN )
-    GPIO.setup( NEXT_MOVE_PIN, GPIO.IN )
+    setupButton( PREV_MOVE_PIN ) 
+    setupButton( NEXT_MOVE_PIN )
+    setupButton( WAGON_SELECT_PIN )
+    setupButton( WAGON_CHANGE_PIN )
+    setupButton( SHUTDOWN_PIN )
+
     pygame.init()
+    pygame.mouse.set_visible( False )
 
     game = Game( sys.argv[1] )
     game.start()
